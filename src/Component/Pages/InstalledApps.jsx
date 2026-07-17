75dd7a6a506055a0619c13ad89d02736a7be4ac3
MD Fahim Pathan <mdfahimpathan61@gmail.com>
2026-07-17 08:37:26 +0600

    implemant sort and uninstall feature in installation folder

--- src/Component/Pages/InstalledApps.jsx
+++ src/Component/Pages/InstalledApps.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
-import { useLoaderData } from 'react-router';
-import { getId } from './../../utilities/addToDB'
-import { Download, Star, UserStar } from 'lucide-react';
+
+import { Link, useLoaderData } from 'react-router';
+import { getId, deleteId } from './../../utilities/addToDB'
+import { Download, Star } from 'lucide-react';
+import { useState } from 'react';
 
 const InstalledApps = () => {
     //const appsData = useLoaderData()
     const storedId = getId()
     const allApps = useLoaderData()
 
+
     const installedApps = storedId.map((id) => {
         const installedApp = allApps.find(app => app.id == id)
         return installedApp
     })
     console.log(installedApps)
+    const [installedAppState, setInstalledAppState] = useState(installedApps)
+    const handleSort = () => {
+        const sortedApps = installedApps.sort((a, b) => a.size - b.size)
+        setInstalledAppState(sortedApps)
+    }
+
+    const handleUninstall = (id) => {
+        const remainingApps = installedAppState.filter(app => app.id != id)
+        setInstalledAppState(remainingApps)
+    }
 
     return (
 
@@ -28,14 +40,20 @@ const InstalledApps = () => {
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn m-1">Sort By</div>
                         <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded z-1 w-20 p-2 shadow-sm">
-                            <li><a>Size</a></li>
-                            
+                            <li onClick={() => handleSort()}><a>Size</a></li>
+
                         </ul>
                     </div>
                 </div>
                 <div className='lg:max-w-360 mx-auto pb-4'>
-                    {
-                        installedApps.map(app => {
+                    {installedApps.length == 0 ?
+                        <div className='text-center py-7 '>
+                            <h1 className='text-red-400 text-2xl font-bold'>No Installed App</h1>
+                            <Link to={"/apps"}>
+                            <button   className='btn bg-linear-to-r from-[#632EE3] to-[#9F62F2] py-2 px-4 text-white mt-3'>Go Back</button>
+                        </Link>
+                        </div> :
+                        installedAppState.map(app => {
                             return <div className='md:flex justify-between bg-white mt-3  mx-4 items-center p-4' >
                                 <div className='flex gap-4 items-center '>
                                     <div><img className='h-20 rounded-lg' src={app.image} alt="" /></div>
@@ -61,7 +79,7 @@ const InstalledApps = () => {
                                 </div>
 
                                 <div className='text-center mt-3 md:mt-0 '>
-                                    <button className='btn text-white p-2 bg-green-400 w-full'>UnInstall </button>
+                                    <button onClick={() => { deleteId(app.id), handleUninstall(app.id) }} className='btn text-white p-2 bg-green-400 w-full'>UnInstall </button>
                                 </div>
                             </div>
                         })
--- src/utilities/addToDB.js
+++ src/utilities/addToDB.js
@@ -1,3 +1,11 @@
+
+export const deleteId = (appId) =>{
+    const ids = JSON.parse(localStorage.getItem("Id"));
+    const updatedId = ids.filter(id => appId != id)
+    localStorage.setItem("Id",JSON.stringify(updatedId))
+
+}
+
 export const getId = () =>{
    const storedIds = localStorage.getItem("Id")
     if(storedIds){
